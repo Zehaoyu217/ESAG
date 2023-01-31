28ece06e26f7737edf66ad9a80134cf618e75f3e
Zehaoyu217 <123233654+Zehaoyu217@users.noreply.github.com>
2023-01-31 00:28:20 -0500

    Add files via upload

--- ESAG.R
+++ ESAG.R
@@ -2,27 +2,27 @@
 library("optimx")
 library("MASS")
 
-#construct orthonormal basis 
+#' Function to construct orthonormal basis that contains the unit vector mu/|mu|
+#'
+#' @param mu non-zero vector with length d
+#'
+#' @return d*d matrix that columns are the orthonormal basis, and the last column is mu/|mu|
+#'
 ONB=function(mu){
   d=length(mu)
   u=matrix(NA,d,d)
-  
   u[,1]=mu 
   u[,2]=c(-mu[2],mu[1],rep(0,(d-2)))
   
   for(i in 3:d){
-    #u_i=c(rep(1,i-1),rep(0,d-i+1))
-    #u_i=mu*u_i
-    #u_i=u_i*mu[i]
     u_i = mu*c(rep(1,i-1),rep(0,d-i+1))*mu[i]
     u_i[i]=-sum(mu[1:(i-1)]^2)
     u[,i]=u_i
   }
+  
   V=matrix(NA,d,d)
   std = function(x){return(x/sqrt(sum(x^2)))}
   V = apply(u,2,std)
-  #K=V[,2:d]
-  #K = cbind(K,V[,1]) #to make mu/||mu|| to be v_d
   K = cbind(V[,2:d],V[,1])
   
   ord = which(is.na(colSums(K))) #to find out columns that are NA (indicating mu_1,...,mu_k = 0 for k>=2) 
@@ -34,11 +34,16 @@ ONB=function(mu){
 }
 
 
+#' Function that map parameter gamma to (lambda, theta, phi)
+#'
+#' @param gamma vector that d = (1+(1+8*(1+g))^0.5)/2 is an integer, where g = length(gamma)
+#'
+#' @return list that contains a vector of lambda, a vector of theta, and a vector of phi
+#'
 parameter=function(gamma){
-  
   g=length(gamma)
   d=(1+sqrt(1+8*(1+g)))/2
-  
+  if(as.integer(d) != d){return('incorrect dimensions of the input vector')}
   lambda=rep(NA,(d-1))
   
   K=rep(NA,(d-2)) #creating K for lambda_i=K_i*lambda_{i-1}
@@ -48,9 +53,6 @@ parameter=function(gamma){
   
   K[1]=sqrt((gamma[1]^2)+(gamma[2]^2))+1 #calclate K_1 
   theta[1]=atan2(gamma[2],gamma[1]) #calculate theta1
-  #if(gamma[1]^2+gamma[2]^2==0){theta[1]=0}
-  #else if(gamma[2]>=0){theta[1] = acos(gamma[1]/sqrt(gamma[1]^2+gamma[2]^2))}
-  #else if(gamma[2]< 0){theta[1] = -acos(gamma[1]/sqrt(gamma[1]^2+gamma[2]^2))}
   
   counting=function(n){ 
     return(n*(n+1)/2)} #creating a counting function
@@ -72,27 +74,26 @@ parameter=function(gamma){
         else{phi_group[j] = acos(group[j] / sqrt(sum(group[j:n]^2)))}  
       }
       phi=c(phi,phi_group)
-      
-      #theta
-      #if((group[(n-1)]^2+group[n]^2)==0){ theta[i]=0 }
-      #else if(group[n]>=0){theta[i] = acos(group[(n-1)]/sqrt(group[(n-1)]^2+group[n]^2))}
-      #else if(group[n] <0){theta[i] = -acos(group[(n-1)]/sqrt(group[(n-1)]^2+group[n]^2))}
       theta[i] = atan2(group[(n)],group[(n-1)])
     }
   }
   
   K_prod =prod(K^seq((d-2) ,1))
-  
   lambda[1]=(1/K_prod)^(1/(d-1))
   for(j in 2:(d-1)){
     lambda[j]=K[(j-1)]*lambda[j-1]
   }
-  
   return(list(lambda,theta,phi))
 }
 
 
-#Rotation Matrix
+
+#' Function to create Rotation Matrix
+#'
+#' @param gamma a vector that d = (1+(1+8*(1+g))^0.5)/2 is an integer, where g = length(gamma)
+#'
+#' @return Rotation mapping, (d-1)*(d-1) matrix 
+#'
 rotation=function(gamma){
   para = parameter(gamma)
   theta = para[[2]]  
@@ -131,8 +132,14 @@ rotation=function(gamma){
 }
 
 
-
-# Covariance Matrix
+#' Function to create Variance-Covariance Matrix, V, in ESAG(mu,V)
+#'
+#' @param mu mean direction, non-zero vector with length d
+#' @lambda eigenvalues of V, vector of positive lambdas with length d-1, generated from function parameter(gamma)
+#' @R Rotation mapping, (d-1) by (d-1) matrix generated from function rotation(gamma)
+#'
+#' @return d by d matrix
+#'
 covariance_matrix=function(mu,lambda,R){
   d=length(mu)
   eigenvector_hat=ONB(mu)
@@ -142,6 +149,15 @@ covariance_matrix=function(mu,lambda,R){
   return(V)
 }
 
+
+#' Function to create Variance-Covariance Matrix, V^0.5, in ESAG(mu,V)
+#'
+#' @param mu mean direction, non-zero vector with length d
+#' @lambda eigenvalues of V, vector of positive lambdas with length d-1, generated from function parameter(gamma)
+#' @R Rotation mapping, (d-1) by (d-1) matrix generated from function rotation(gamma)
+#'
+#' @return d by d matrix
+#'
 covariance_matrix_2=function(mu,lambda,R){
   d=length(mu)
   eigenvector_hat=ONB(mu)
@@ -151,7 +167,14 @@ covariance_matrix_2=function(mu,lambda,R){
   return(V)
 }
 
-
+#' Function to create inverse of Variance-Covariance Matrix, V^-1, in ESAG(mu,V)
+#'
+#' @param mu, mean direction, non-zero vector with length d
+#' @param lambda, eigenvalues of V, vector of positive lambdas with length d-1, generated from function parameter(gamma)
+#' @param R, Rotation mapping, (d-1)*(d-1) matrix generated from function rotation(gamma)
+#'
+#' @return d by d matrix
+#'
 covariance_inv_matrix=function(mu,lambda,R){
   d=length(mu)
   eigenvector_hat=ONB(mu)
@@ -161,8 +184,14 @@ covariance_inv_matrix=function(mu,lambda,R){
   return(V)
 }
 
-
-#ESAG generators
+#' Function to generate ESAG random variables
+#'
+#' @param n, number of ESAG random variables, numeric 
+#' @param mu, mean direction, non-zero vector with length d
+#' @param V, variance-covariance matrix, d*d squared matrix 
+#'
+#' @return n ESAG random variables, n by d matrix
+#'
 ESAG_generator=function(n,mu,V){
   if(n == 1){
     X=mvrnorm(1,mu,V)
@@ -177,7 +206,14 @@ ESAG_generator=function(n,mu,V){
   }
 }
 
-#ESAG under reparameters and correct conditions
+#' Function to generate ESAG random variables
+#'
+#' @param n, number of ESAG random variables, numeric 
+#' @param mu, mean direction, non-zero vector with length d
+#' @param gamma, vector with length g = (d-2)*(d+1)/2
+#'
+#' @return n ESAG random variables, n by d matrix
+#'
 ESAG=function(n,mu,gamma){
   lambda=parameter(gamma)[[1]]
   R=rotation(gamma)
@@ -186,8 +222,14 @@ ESAG=function(n,mu,gamma){
   return(Y)
 }
 
-
-#Model Diagnostic T1 version for graphic diagnostic
+#' Function to generate T1 version of residuals given mu, gamma, and the data Y
+#'
+#' @param mu, mean direction, non-zero vector with length d
+#' @param gamma, vector with length g = (d-2)*(d+1)/2
+#' @param Y, n by d matrix, n observations of d-dimension ESAG 
+#'
+#' @return T1 residuals, vector with length n
+#'
 Model_diagnostic_mu = function(mu,gamma,Y){
   n = nrow(Y)
   d = length(mu)
@@ -203,13 +245,19 @@ Model_diagnostic_mu = function(mu,gamma,Y){
   return(chi_sq)
 }
 
-#Model Diagnostic for numerical bootstrap gof
+#' Function to generate residuals, Q, given mu, gamma, and the data Y
+#'
+#' @param mu, mean direction, non-zero vector with length d
+#' @param gamma, vector with length g = (d-2)*(d+1)/2
+#' @param Y, n by d matrix, n observations of d-dimension ESAG 
+#'
+#' @return Q residuals, vector with length n
+#'
 Model_diagnostic_Q = function(mu,gamma,Y){
   n = nrow(Y)
   d = length(mu)
   chi_sq = rep(0,n)
   for(i in 1:n){
-    #mu_i = t(t(mu)*X[i])
     mu_norm = sqrt(sum(mu^2))
     lambda = parameter(gamma)[[1]]
     R = rotation(gamma)
@@ -221,8 +269,13 @@ Model_diagnostic_Q = function(mu,gamma,Y){
 }
 
 
-
-# log likelihood function
+#' log likelihood function
+#'
+#' @param Bx_i, coefficients at covariates x_i, vector with length (d+2)(d-1)/2
+#' @param y_i, vector with length d
+#'
+#' @return log-likelihood, numeric
+#'
 log_likelihood=function(Bx_i,y_i){  
   d=length(y_i)
   mu=Bx_i[1:d]
@@ -243,45 +296,18 @@ log_likelihood=function(Bx_i,y_i){
    }
   }
   M_p=Mp[p]
-  
-  # M = function(u){
-  #   alpha = as.vector( t(y_i)%*%mu/(yVy^(1/2)) )
-  #   return(u^{d-1}*(2*pi)^(-0.5)*exp(-(u-alpha)^2/2))
-  # }
-  # M_p = integrate(f=M,lower=0,upper=Inf)$value
-  
-  
   f=log(Cd)-(d/2)*log(yVy)+(1/2)*((t(y_i)%*%mu)^2/yVy - t(mu)%*%mu)+log(M_p) 
   return(f)
 }
 
-# restricted log likelihood function isotropic
-log_likelihood_isotropic_restricted=function(Bx_i,y_i){  
-  d=length(y_i)
-  mu=Bx_i
-  #gamma=Bx_i[-(1:d)]
-  #R=rotation(gamma)
-  #lambda=parameter(gamma)[[1]]
-  #V_inv=covariance_inv_matrix(mu,lambda,R)
-  V_inv = diag(1,d,d)
-  p=d-1
-  yVy=t(y_i)%*%V_inv%*%y_i
-  a=as.vector( t(y_i)%*%mu/(yVy^(1/2)) )
-  Cd = (2*pi)^(-p/2)
-  Mp=rep(0,p)
-  Mp[1]=a*pnorm(a)+dnorm(a)
-  Mp[2]=(1+a^2)*pnorm(a)+a*dnorm(a)
-  if(p>=3){
-    for(i in 3:p){
-      Mp[i]=a*Mp[(i-1)]+(i-1)*Mp[(i-2)]
-    } 
-  }
-  M_p=Mp[p]
-  f=log(Cd)-(d/2)*log(yVy)+(1/2)*((t(y_i)%*%mu)^2/yVy - t(mu)%*%mu)+log(M_p) 
-  return(f)
-}
-
-# Bootstrap GOF
+#' Goodness of Fit test with bootstrap, non-regression version
+#'
+#' @param MLE, maximum likelihood estimations given the data Y, vector with length (d+2)(d-1)/2
+#' @param Y, dataset with n observations, n by d matrix
+#' @param B, number of bootstrap sampling, numeric
+#'
+#' @return p-values, numeric
+#'
 Bootstrap_gof = function(MLE,Y,B){  #B is # of bootstrap MC replicas
   k=nrow(Y)
   B_dim=length(MLE)
@@ -295,9 +321,6 @@ Bootstrap_gof = function(MLE,Y,B){  #B is # of bootstrap MC replicas
     B_dim=length(B)
     d=length(y)
     y = as.matrix(y,length(y),1)
-    #B0=B[1:B_dim]
-    #B1=B[(B_dim+1):length(B)]
-    #Bx=B0+B1*x
     Bx = B
     mu=Bx[1:d]
     gamma=Bx[(d+1):B_dim]
@@ -347,19 +370,23 @@ Bootstrap_gof = function(MLE,Y,B){  #B is # of bootstrap MC replicas
   }
   
   residual_Q = Model_diagnostic_Q(mu=MLE[1:4],gamma=MLE[5:9],Y=Y)
-  
   Y_copy = ESAG(k,mu=MLE[1:4],gamma=MLE[5:9])
   residual_Q_copy = Model_diagnostic_Q(mu=MLE[1:4],gamma=MLE[5:9],Y=Y_copy)
   p_Q = ks.test(residual_Q,residual_Q_copy)$p.value
   real_p_Q = mean(pvalue_Q < p_Q)
-  
   return(real_p_Q )
 }
 
-
-
-
-MLE = function(B_start=c(), Y, X=c()){
+#' Function to find MLEs
+#'
+#' @param B_start, starting point of the coefficient, ((d+2)(d-1)/2) by p matrix
+#' @param Y, dataset with n observations, n*d matrix
+#' @param X, covariates, n*p matrix, p is number of covariates
+#' @param H, TRUE/FALSE for Hessian matrix in the output
+#'
+#' @return list that contains a vector of MLEs, maxmimal log-likelihood value, etc from function optim() with 'BFGS'. It can output Hessian matrix as well
+#'
+MLE = function(B_start=c(), Y, X=c(), H = FALSE){
   if(is.null(X)){
     X = as.matrix(rep(1,nrow(Y)))
   } else{ X = cbind(rep(1,nrow(Y)),X) }
@@ -391,50 +418,7 @@ MLE = function(B_start=c(), Y, X=c()){
       return(sum(f))
     }
     start = B
-    op = optim(par=start, joint_log_likelihood, control=list(fnscale=-1,maxit=10000), method="BFGS")
+    op = optim(par=start, joint_log_likelihood, control=list(fnscale=-1,maxit=10000), method="BFGS", hessian = H)
     return(op)
   }
-}
-
-
-MLE_isotropic_restricted = function(B_start=c(), Y, X=c()){
-  if(is.null(X)){
-    X = as.matrix(rep(1,nrow(Y)))
-  }
-  else{ X = cbind(rep(1,nrow(Y)),X) }
-  
-  if(is.null(B_start)){
-    B_start = matrix(1,ncol(Y),ncol(X))
-  }
-  
-  if(ncol(B_start) != (ncol(X)) ){
-    return("number of covariates in B and X do not match")
-  }
-  else if( nrow(B_start) !=  ncol(Y) ){
-    return("dimension of B do not match with dimension of Y")
-  }
-  else if(nrow(Y) != nrow(X)){
-    return("number of observations of X and Y do not match")
-  }
-  else{
-    B = c(B_start)
-    joint_log_likelihood=function(B,y=Y,x=X){
-      n = nrow(y)
-      d = ncol(y)
-      B_dim = d
-      f=rep(0,n)
-      for(i in 1:n){
-        Bx = t(matrix(B,nrow=B_dim) %*% t(x))
-        f[i]=log_likelihood_isotropic_restricted(Bx_i = Bx[i,] ,y[i,])
-      }
-      return(sum(f))
-    }
-    start = B
-    op = optim(par=start, joint_log_likelihood, control=list(fnscale=-1,maxit=10000), method="BFGS")
-    return(op)
-  }
-}
-
-std = function(x){
-  return( 1 + (x - min(x))/(max(x)-min(x)) )
 }
